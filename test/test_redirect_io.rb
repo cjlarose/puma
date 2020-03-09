dba03933679e900e428c0956f985602afbe52856
Chris LaRose <cjlarose@gmail.com>
2020-03-08 23:26:49 -0700

    Add test for redirect_io for single

---
+++ test/test_redirect_io.rb
@@ -0,0 +1,68 @@
+require_relative "helper"
+require_relative "helpers/integration"
+
+class TestRedirectIO < TestIntegration
+  # parallelize_me!
+
+  def setup
+    super
+
+    @out_file = Tempfile.new 'puma-out'
+    @out_file_path = @out_file.path
+    @err_file = Tempfile.new 'puma-err'
+    @err_file_path = @err_file.path
+  end
+
+  def teardown
+    super
+
+    @out_file.close
+    @err_file.close
+    File.unlink @out_file_path
+    File.unlink @err_file_path
+  end
+
+  def test_sighup_redirects_io_single
+    skip_unless_signal_exist? :HUP
+
+    cli_args = [
+      '--redirect-stdout', @out_file_path,
+      '--redirect-stderr', @err_file_path,
+      'test/rackup/hello.ru'
+    ]
+    cli_server cli_args.join ' '
+
+    log_rotate_output_files
+
+    Process.kill :HUP, @server.pid
+
+    wait_until_file_has_content @out_file
+    assert_match 'puma startup', @out_file.readline
+
+    wait_until_file_has_content @err_file
+    assert_match 'puma startup', @err_file.readline
+  end
+
+  private
+
+  def log_rotate_output_files
+    # rename both files to .old
+    old_out_file_path = "#{@out_file_path}.old"
+    old_err_file_path = "#{@err_file_path}.old"
+    File.rename @out_file_path, old_out_file_path
+    File.rename @err_file_path, old_err_file_path
+
+    # reload references to output files
+    @out_file.close
+    @err_file.close
+    @out_file = File.open @out_file_path, File::RDONLY | File::CREAT
+    @err_file = File.open @err_file_path, File::RDONLY | File::CREAT
+  end
+
+  def wait_until_file_has_content(file)
+    file.read_nonblock 1
+    file.seek 0
+  rescue EOFError
+    retry
+  end
+end
